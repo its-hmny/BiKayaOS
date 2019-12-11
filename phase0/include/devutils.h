986dc44c0a0ec0a055d1af01588d94e2b7382ccf
Enea Guidi <eneag3@hotmail.com>
2019-12-11 15:12:59 +0100

    Refractored the code to better understanding, the project is tested and work always

--- phase0/include/devutils.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DEVUTILS_H__
-#define __DEVUTILS_H__
-
-void print(char* buffer);
-
-#endif
--- phase0/include/termutils.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TERMUTILS_H__
-#define __TERMUTILS_H__
-
-void term_puts(const char *str);
-void term_gets(char usr_input[], unsigned int STR_LENGHT);
-
-#endif
