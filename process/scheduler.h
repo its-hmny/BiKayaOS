4b704fceeaa415d1c4ba7c3834cd4e82e3dd84de
minosse99 <simoneboldrini1999@gmail.com>
2020-03-21 17:45:54 +0100

    --add the scheduler of OS

---
+++ process/scheduler.h
@@ -0,0 +1,15 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include "../include/types_bikaya.h"
+
+extern struct list_head ready_queue;
+extern struct pcb_t* currentProc;
+
+void scheduler();
+void insertProcReady(struct pcb_t* newProc);
+void removeProcReady(struct pcb_t* rmProc);
+void aging();
+void scheduler_add(pcb_t *process);
+
+#endif
