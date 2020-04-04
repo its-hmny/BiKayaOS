fb5ed6af7ac5b1e121d6c44bdff44cadd6b93484
Enea <eneag3@hotmail.com>
2020-04-04 23:39:29 +0200

    Fixed CMake for uARM, uMPS still work in progress

--- uARM.cmake
+++ uARM.cmake
@@ -5,14 +5,14 @@ set(UARM_SRC ${PROJECT_SOURCE_DIR}/include/uARM/uarm)
 # Flags to be passed to the compiler
 set(CFLAGS_UARM -mcpu=arm7tdmi -O0)
 set(LINK_SCRIPT ${UARM_INC}/elf32ltsarm.h.uarmcore.x)
-# Flags to be passed to the linker. NOTE: the linker is not invoked directly,
-# flags are passed to it via the -Wl compiler flag
+
+# Flags to be passed to the linker. NOTE: the linker is not invoked directly, flags are passed to it via the -Wl compiler flag
 set(LDFLAGS_UARM "-Wl,-nostdlib,-G,0,-T,${LINK_SCRIPT}")
 
 # Set compiler and linker options
 add_compile_options(${CFLAGS_UARM})
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LDFLAGS_UARM}")
-set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
+
 # Specify #include<...> directories to look at
 include_directories(${UARM_INC})
 include_directories(${UARM_INC}/uarm)
@@ -21,5 +21,16 @@ add_library(libuarm ${UARM_INC}/libuarm.s)
 add_library(libdiv ${UARM_INC}/libdiv.s)
 add_library(crtso ${UARM_INC}/crtso.s)
 
-add_executable(kernel.uarm ${SRC}/phase1,5_test.c)
-target_link_libraries(kernel.uarm crtso libuarm termprint)
+add_executable(
+	kernel ${SRC}/phase1,5_test.c ${EXC}interrupt.c ${EXC}/syscall_breakpoint.c
+	${GNR}/utils.c ${PRC}/scheduler.c ${PRC}/pcb.c ${PRC}/asl.c
+)
+
+target_link_libraries(kernel crtso libuarm libdiv)
+
+add_custom_target(
+	kernel.core.uarm ALL
+	COMMAND elf2uarm -k ./kernel
+	BYPRODUCTS kernel.core.uarm kernel.stab.uarm
+	DEPENDS ./kernel
+)
--- uMPS.cmake
+++ uMPS.cmake
@@ -3,18 +3,15 @@ set(UMPS_INC ${PROJECT_SOURCE_DIR}/include/uMPS)
 set(UMPS_SRC ${PROJECT_SOURCE_DIR}/include/uMPS/umps)
 
 # Flags to be passed to gcc
-set(
-	CFLAGS_UMPS
-	-ffreestanding -ansi -mips1 -mabi=32 -mno-gpopt -G 0 # -mabicalls -fPIC
-	-mfp32 -Wall -O0
-)
+set(CFLAGS_UMPS	-ffreestanding -mips1 -mabi=32 -mno-gpopt -G 0 -mfp32 -Wall -O0)
 set(LINK_SCRIPT ${UMPS_INC}/umpscore.ldscript)
+
 # Flags to be passed to ld
 set(LDFLAGS_UMPS "-nostdlib -Wl,-G,0,-nostdlib,-T,${LINK_SCRIPT}")
 
 add_compile_options(${CFLAGS_UMPS})
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LDFLAGS_UMPS}")
-# set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
+
 # Specify #include<...> directories to look at
 include_directories(${UMPS_INC})
 include_directories(${UMPS_INC}/umps)
@@ -22,13 +19,17 @@ include_directories(${UMPS_INC}/umps)
 add_library(crtso ${UMPS_INC}/crtso.S)
 add_library(libumps ${UMPS_INC}/libumps.S)
 
-add_executable(kernel ${SRC}/phase1,5_test.c)
-target_link_libraries(kernel crtso libumps termprint)
+add_executable(
+	kernel ${SRC}/phase1,5_test.c ${EXC}/interrupt.c ${EXC}/syscall_breakpoint.c
+	${GNR}/utils.c ${PRC}/scheduler.c ${PRC}/pcb.c ${PRC}/asl.c
+)
+
+target_link_libraries(kernel crtso libumps)
 
 # Run the `umps2-elf2umps -k kernel' command after building `kernel'
 add_custom_target(
 	kernel.core.umps ALL
-	COMMAND umps2-elf2umps -k ${PROJECT_BINARY_DIR}/kernel
+	COMMAND umps2-elf2umps -k ./kernel
 	BYPRODUCTS kernel.core.umps kernel.stab.umps
-	DEPENDS kernel
+	DEPENDS ./kernel
 )
