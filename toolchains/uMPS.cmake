935d754685fe794b6efe745aa40b7a66cc237894
Enea Guidi <hmny@athena.homenet.telecomitalia.it>
2020-04-13 17:05:45 +0200

    Updated gitignore

--- toolchains/uMPS.cmake
+++ toolchains/uMPS.cmake
@@ -2,6 +2,6 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR mips)
 
 # Set the compiler and the linker 
-set(CMAKE_C_COMPILER mipsel-unknown-linux-gnu-gcc)
-set(CMAKE_ASM_COMPILER mipsel-unknown-linux-gnu-gcc)
-set(CMAKE_C_LINKER mipsel-unknown-linux-gnu-ld)
+set(CMAKE_C_COMPILER /home/hmny/install/x-tools/mipsel-unknown-linux-gnu/bin/mipsel-unknown-linux-gnu-gcc)
+set(CMAKE_ASM_COMPILER /home/hmny/install/x-tools/mipsel-unknown-linux-gnu/bin/mipsel-unknown-linux-gnu-gcc)
+set(CMAKE_C_LINKER /home/hmny/install/x-tools/mipsel-unknown-linux-gnu/bin/mipsel-unknown-linux-gnu-ld)
