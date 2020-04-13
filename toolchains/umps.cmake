0a417c0299df3c28169ed625d4230f34ee48e8a5
MarcoTomasone <50140370+MarcoTomasone@users.noreply.github.com>
2020-04-13 15:23:16 +0200

    Fixed gcc to ld in linker

--- toolchains/umps.cmake
+++ toolchains/umps.cmake
@@ -4,4 +4,4 @@ set(CMAKE_SYSTEM_PROCESSOR mips)
 # Set the compiler and the linker 
 set(CMAKE_C_COMPILER mipsel-unknown-linux-gnu-gcc)
 set(CMAKE_ASM_COMPILER mipsel-unknown-linux-gnu-gcc)
-set(CMAKE_C_LINKER mipsel-unknown-linux-gnu-gcc)
+set(CMAKE_C_LINKER mipsel-unknown-linux-gnu-ld)
