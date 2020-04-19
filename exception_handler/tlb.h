1ce90bfbf04953f6f9095adf644b8ad259a92c13
minosse99 <simoneboldrini1999@gmail.com>
2020-04-19 17:00:09 +0200

    update tlb source file and add exception type on types_bikaya

--- exception_handler/tlb.h
+++ exception_handler/tlb.h
@@ -1,6 +1,13 @@
 #ifndef __TLB_H
 #define __TLB_H
 
+#include "../generics/utils.h"
+#include "../process/scheduler.h"
+#include "syscall_breakpoint.h"
+#include "../include/types_bikaya.h"
+#include "../include/system_const.h"
+#include "tlb.h"
+
 void tlb_handler(void);
 
 #endif
