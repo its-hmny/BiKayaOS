1cddfa990abe0cfc8f345fd6df6e1c97cf704277
minosse99 <simoneboldrini1999@gmail.com>
2020-04-19 18:21:06 +0200

    update trap.* files

--- exception_handler/trap.c
+++ exception_handler/trap.c
@@ -4,6 +4,19 @@
 state_t *oldarea = NULL;
 
 void trap_handler(void) {
-    oldarea = OLD_AREA_TRAP;
+    oldarea = OLD_AREA_TRAP;    
+//  old_Area->pc_epc += WORDSIZE;
+    pcb_t* currentProcess = getCurrentProc();
+
+    int code = (currentProcess->custom_hndlr).has_custom_handler[2];
+    if(!code){  //if code == 0 then we don't considerer tlb exception instead  
+                //if code == true we must terminate process
+        syscallDispatcher(3); 
+        
+    }
+    //BreakpointTrapHandler()   <---  ???
+    clonestate(oldarea,(currentProcess->custom_hndlr).trap_old,sizeof(oldarea));
+    LDST((currentProcess->custom_hndlr).trap_new);
+   
     PANIC();
 }
--- exception_handler/trap.h
+++ exception_handler/trap.h
@@ -1,6 +1,14 @@
 #ifndef __TRAP_H
 #define __TRAP_H
 
+#include "../generics/utils.h"
+#include "../process/scheduler.h"
+#include "syscall_breakpoint.h"
+#include "../include/types_bikaya.h"
+#include "../include/system_const.h"
+#include "tlb.h"
+
+
 void trap_handler(void);
 
 #endif
